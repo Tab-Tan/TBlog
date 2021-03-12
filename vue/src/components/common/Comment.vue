1f8607045bba7e8b420d71aec79615c01a26caab
Tab-tan <1607847510@qq.com>
2021-03-12 19:33:38 +0800

    添加了修改主题背景和主页欢迎公告

---
+++ vue/src/components/common/Comment.vue
@@ -0,0 +1,129 @@
+<template>
+  <!--评论-->
+  <div class="comment">
+    <!--已有评论-->
+    <div class="comments-panel">
+      <span>已有评论: {{comments.length}}条</span>
+      <hr>
+      <div class="comment-item" v-for="comment in comments">
+        <img :src="comment.user.headImg" width="40px" alt="头像" />
+        <div class="ib">
+          <!--用户昵称-->
+          <a href="javascript:;" v-text="comment.user.nikeName"></a>
+          <!--回复按钮 和 时间-->
+          <div class="comment-tools">
+            <a href="javascript:;" class="btn-hf">回复</a>
+            <span v-text="comment.date"></span>
+          </div>
+          <!--评论内容-->
+          <div class="comment-content">
+            <p v-text="comment.comment"></p>
+          </div>
+        </div>
+      </div>
+      <div class="comment-sub">
+        <textarea rows="6" placeholder="请输入评论..." v-model="commentTemp.comment"></textarea>
+        <a class="btn-simple" @click="sub">提交</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Comment",
+  data(){
+    return{
+      comments: [
+        {id: 1,date: new Date(Date.now()),user:{id:1,headImg:'http://q1.qlogo.cn/g?b=qq&nk=1607847510&s=640',nikeName: 'TabTan'},comment: '哇哦，残疾的我看完之后飞檐走壁!',reply: {}},
+        {id: 1,date: new Date(Date.now()),user:{id:1,headImg:'http://q1.qlogo.cn/g?b=qq&nk=1607847501&s=640',nikeName: 'TabTan'},comment: '哇塞，我来占个楼！！！!'}
+      ],
+      commentTemp: {id: 1,date: new Date(Date.now()),user:{id:1,headImg:'http://q1.qlogo.cn/g?b=qq&nk=1607847501&s=640',nikeName: 'TabTan'},comment: '请输入评论~@^-^@~'}
+    }
+  },
+  methods: {
+    sub(){
+      this.comments.push(this.commentTemp)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .comment{
+    margin-top: 40px;
+  }
+  .comment-item{
+    width: 960px;
+    margin-left: 30px;
+    border-bottom: 1px #757373 solid;
+  }
+  .comments-panel{
+    background: rgba(255,255,255,.6);
+    /*background-color: #ecf0f3;*/
+    padding: 10px;
+    margin-bottom: 40px;
+    box-sizing: border-box;
+    box-shadow: 8px 8px 15px #c6c4c4,-8px -8px 15px #FFFFFF;
+  }
+  .comment textarea{
+    margin-top: 30px;
+    width: 940px;
+    /*border: 4px #05b076 solid;*/
+    border: none;
+    border-radius: 10px;
+    padding: 20px;
+    background-color: #ecf0f3;
+    box-shadow: inset 10px 10px 15px #dbdbdb,inset -10px -10px 15px #FFFFFF;
+    resize: none;
+  }
+  img{
+    border-radius: 3px;
+    margin: 6px;
+    float: left;
+  }
+  .ib{
+    margin: 4px;
+  }
+  .comment-tools{
+    margin: 4px;
+  }
+  .comment-content{
+    margin: 10px 4px;
+  }
+  p:hover{
+    /*box-shadow: 1px 1px 2px #797a7c;*/
+    -moz-box-shadow:2px 2px 5px #333333;
+    -webkit-box-shadow:2px 2px 5px #333333;
+    box-shadow:2px 2px 5px #333333;
+    transition: box-shadow .3s;
+    cursor: pointer;
+  }
+  .comment-sub a{
+    width: 50px;
+    margin: 5px;
+    display: block;
+    text-align: center;
+    color: #8ad49a;
+    border-color: #8ad49a;
+  }
+  .comment-sub a:hover{
+    color: #05b076;
+    border-color: #42b983;
+    transition: 0.7s;
+    cursor: pointer;
+  }
+  .btn-hf{
+    width: 90px;
+    border: 1px #c6c4c4 solid;
+    margin-top: 10px;
+    text-align: left;
+    padding: 4px 8px;
+    border-radius: 4px;
+  }
+  .btn-hf:hover{
+    /*border-color: #05b076;*/
+    background-color: #bff5dd;
+    transition: .2s;
+  }
+</style>
---
+++ vue/src/components/common/Header.vue
@@ -0,0 +1,148 @@
+<template>
+  <!--头部导航-->
+  <div class="header-center">
+    <!--导航-->
+    <div class="header">
+      <div class="nikeName left"><span> {{ nikeName }}</span></div>
+      <div class="right">
+        <ul>
+          <a href="#/"><li class="li-h">首页</li></a>
+          <li id="link" class="li-h">
+            常用
+            <ul>
+              <li v-for="link in links" class="li-h">{{ link }}</li>
+            </ul>
+          </li>
+          <a href=""><li class="li-h">新随笔</li></a>
+          <a href="" @click.prevent="login"><li class="li-h">登录</li></a>
+          <a href="" @mouseover.prevent="roomTip" @click.prevent="roomIn" id="h-room"><li class="li-h">聊天室</li></a>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import axios from "axios";
+export default {
+  name: "Header",
+  data(){
+    return{
+      nikeName: "TBlog",
+      links: ['github','码云'],
+      codeAge: '1年6个月',
+      fansNum: 9,
+      forksNum: 5,
+      classifies: [
+        {classify:'Linux',count: 14},
+        {classify:'java基础',count: 20},
+        {classify:'设计模式',count: 23},
+        {classify:'虚拟机',count: 11},
+        {classify:'spring全家桶',count: 44}
+      ]
+    }
+  },methods:{
+    login(){
+      this.$router.push({name:"Login"})
+    },
+    roomTip(){
+      layer.tips('进来聊天呀', '#h-room', {
+        tips: [2, '#35a058']
+      });
+    },
+    roomIn(){
+      layer.open({
+        type: 2,
+        title: '聊天室',
+        area: ['693px','510px']
+        ,offset: 'rt' //具体配置参考：offset参数项
+        ,content: '#/room'
+        ,btnAlign: 'c' //按钮居中
+        ,shade: 0 //不显示遮罩
+        ,yes: function(){
+          layer.closeAll();
+        }
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+/*头部居中弹性盒子*/
+.header-center{
+  display: flex;
+  justify-content: center;
+  position: fixed;
+}
+/*头部盒子*/
+.header{
+  width: 1000px;
+  height: 76px;
+  margin-bottom: 10px;
+  /*background-color: #ecf0f3;*/
+  /*border-bottom: 1px #6a6b6a solid;*/
+  background: rgba(255,255,255,.6);
+  box-shadow: 10px 10px 15px #dedede,-10px -10px 15px #ffffff;
+}
+ul{
+  list-style: none;
+}
+
+.li-h{
+  color: #056e16;
+  margin-right: 30px;
+  display: inline-block;
+  font-size: 17px;
+  padding: 19px 16px;
+}
+.li-h:hover{
+  /*color: #046e18;*/
+  /*border-bottom: 10px #046e18 solid;*/
+  box-sizing: border-box;
+  border-radius: 5px;
+  box-shadow: 8px 8px 4px #d9d9d9,-8px -8px 4px #ffffff;
+  /*border-bottom-left-radius: 10px;*/
+  /*border-bottom-right-radius: 10px;*/
+  transition: .3s;
+}
+
+.nikeName{
+  /*border: 1px red solid;*/
+  color: #05b076;
+  width: 100px;
+  font-size: 45px;
+  display: inline-block;
+  margin: 10px 0;
+}
+.nikeName:hover{
+  color: #b8d749;
+  transition: 1s;
+}
+#link{
+  cursor: url("https://gitee.com/Tab-Tan/CDN/raw/9e49252ef052c52f5bcbf1f77d804ba88ab5deb5/active.ico"),pointer;
+  position: relative;
+
+}
+#link ul{
+  z-index: 1000;
+  border: 1px #c6c4c4 solid;
+  position: absolute;
+  width: 100px;
+  left: -1px;
+  /*background-color: #303030;*/
+  margin-top: 20px;
+  /*background-color: #ecf0f3;*/
+  /*border-bottom: 1px #6a6b6a solid;*/
+  box-shadow: 10px 10px 15px #dedcdc,-10px -10px 15px #ffffff;
+  display: none;
+}
+#link:hover ul{
+  display: block;
+  transition: color 1s;
+}
+#link ul li{
+  transition: 0.7s;
+}
+</style>
